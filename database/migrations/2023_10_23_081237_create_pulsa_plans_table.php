5a9b0a9d6fbb4e1f86a25cee921a0a3fb8cb0d95
agusanwar <agusanwar@gmail.com>
2023-10-23 15:17:26 +0700

    set up project, migration database

---
+++ database/migrations/2023_10_23_081237_create_pulsa_plans_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pulsa_plans', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('price');
+            $table->foreignId('vendor_card_id')->constrained('vendor_cards');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pulsa_plans');
+    }
+};
